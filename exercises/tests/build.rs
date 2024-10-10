8464c228cee087b8d6c28f90f1d2e72cec925c65
EasonLotus <measonkk@outlook.com>
2024-10-10 19:21:25 +0800

    小错误

--- exercises/tests/build.rs
+++ exercises/tests/build.rs
@@ -1,7 +1,8 @@
 //! This is the build script for both tests7 and tests8.
 //!
 //! You should modify this file to make both exercises pass.
-use std::time::{SystemTime+UNIX_EPOCH};
+use std::time::SystemTime;
+use std::time::UNIX_EPOCH;
 fn main() {
     // In tests7, we should set up an environment variable
     // called `TEST_FOO`. Print in the standard output to let
